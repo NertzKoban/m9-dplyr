5240ee623d01ee5c1687d09ae462c73df61ec3ef
Ned Sander <nertzkoban@gmail.com>
2016-10-20 14:32:44 -0700

    finished exercise 5, worked on most of the rest

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,34 +1,37 @@
 # Exercise 4: DPLYR and flights data
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights$air_delay <- (flights$arr_delay - flights$dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, air_delay)
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- mutate(flights, air_delay = arr_delay - dep_delay) %>% 
+  arrange(air_delay)
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$air_delay)
 
 # On average, did flights gain or lose time?
-
+mean(flights$air_delay, na.rm = TRUE) #Lost 5.7 minutes!
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
+to.sea <- flights %>% 
+  select(air_delay, dest) %>% 
+  filter(dest == 'SEA')
 
-
-# On average, did flights to seatac gain or loose time?
-
+# On average, did flights to seatac gain or lose time?
+mean(to.sea$air_delay, na.rm = TRUE) #Lost 11.7 minutes!
 ### Bonus ###
 # Write a function that allows you to specify an origin, a destination, and a column of interest
 # that returns a data.frame of flights from the origin to the destination and only the column of interest
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -7,15 +7,24 @@ library(dplyr)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+dep.delay.monthly <- flights %>% 
+  group_by(month) %>% 
+  summarise(mean_delay = mean(dep_delay, na.rm = TRUE))
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-
+plot(dep.delay.monthly$month, dep.delay.monthly$mean_delay)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+arr.delay.airport <- flights %>% 
+  group_by(dest) %>% 
+  summarise(mean_delay = mean(arr_delay, na.rm = TRUE))
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
+speed.to.airport <- flights %>% 
+  mutate(speed = distance / air_time) %>% 
+  group_by(dest) %>% 
+  summarise(mean_speed = mean(speed, na.rm = TRUE)) %>% 
+  arrange(-mean_speed)
